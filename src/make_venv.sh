fba0ee89df867111c731cae5e5fb439387471522
raykpridgen <rkpridgen@coastal.edu>
2025-10-07 02:21:01 -0400

    Fixed script file to create venv at absolute location

--- src/make_venv.sh
+++ src/make_venv.sh
@@ -1,14 +1,24 @@
-FOLDER_PATH="../../venv"
+#!/bin/bash
+# Where the script itself is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Folder to make venv in
+FOLDER_PATH="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
-if [ -d "$FOLDER_PATH" ]; then
-  source ../../venv/bin/activate
+#echo "$SCRIPT_DIR"
+#echo "$FOLDER_PATH"
+
+# If the actualy activate file exists
+if [ -f "$FOLDER_PATH/venv/bin/activate" ]; then
+  # Activate venv and install dependencies
+  source "$FOLDER_PATH/venv/bin/activate"
   pip3 install zss
   pip3 install matplotlib
   pip3 install igraph
   pip3 install plotly
 else
-  python3 -m venv ../../venv
-  source ../../venv/bin/activate
+  # Make the venv, then continue
+  python3 -m venv "$FOLDER_PATH/venv"
+  source "$FOLDER_PATH/venv/bin/activate"
   pip3 install zss
   pip3 install matplotlib
   pip3 install igraph
